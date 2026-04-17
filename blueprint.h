87f75202b10f089bd0bcd4d8d9d5e714fb335e1c
MalikAbdul4078 <ar3456685@gmail.com>
2026-04-17 10:38:44 +0500

    App class remaining, other classes done

--- blueprint.h
+++ blueprint.h
@@ -1,25 +1,77 @@
 #pragma once 
 #include<iostream>
+#include<string>
 namespace myHeader{
-class user{
-    int ID ;
-    std :: string name ;
-    user** friends ;
-
-
-
+    class page ; // forward declaration 
+    class date {
+        int month , day , year ;
+        public : 
+            date(){}
+            date (int day, int month , int year) ;
+    };
+    class Activity{
+    int type ;
+    int value ;
+    public : 
+    Activity();
+    ~Activity() ;
+    void displayOptions();
+    void setTypeAndValue(int type,std :: string value) ; // sets the value given by the user and the type corresponding to the index in the table 
 };
-class page {
-    int ID ;
-    std :: string description ;
-    int totalLiked ;
-
-
+    class user{
+        std :: string ID ;
+        std :: string name ;
+        user** friends ;
+        page **likedPages ;
+        public : 
+        user();
+        ~user() ;
+        void viewHome() ;
+        void likePost (); 
+        void viewProfile() ;
+        void viewFriendList() ;
 };
-class post {
-    int ID ;
-    user ** liked ;
-    int likedCount ;
-    int commentCount ; 
+    class post {
+        std :: string ID ;
+        std :: string description ;
+        date shareDate ;
+        user ** liked ;
+        Activity *a ; // optional 
+        comment **comments ;
+        int likedCount ;
+        int commentCount ; 
+        public :
+        post() ;
+        ~post() ;
+        void seeLiked() ; // list of people who have liked the post 
+        void addComment(comment c) ;
+        void view() ; 
+    };
+    class page {
+        std :: string ID ;
+        post** posts ;
+        Memory **memories ;
+        user* owner ;
+        int totalLiked ;
+        public : 
+        page() ;
+        ~page();
+        void view() ; // displays all its posts 
+    };
+    class comment{
+    user* commenter ; 
+    std :: string text ;
+    public :
+    comment (user *commenter , std :: string text);
+    ~comment ();
 };
+class Memory{
+    post *ptr ; ;
+    std :: string text ;
+    public : 
+    Memory(post *ptr, std :: string text) ;
+    ~Memory();
+};
+
+class socialNetworkApp ; // this class will serve the purpose of a driver program with the starting point socialNetworkApp :: run() 
 }
