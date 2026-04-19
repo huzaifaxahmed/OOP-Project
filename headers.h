485f2d46650f871b93231710ac24edf6430d014a
huzaifaxahmed <bhhali2013@gmail.com>
2026-04-20 00:17:24 +0500

    Some changes in Headers

--- headers.h
+++ headers.h
@@ -12,18 +12,7 @@ namespace dHeader{
     public : 
         date(){}
         date (int day, int month , int year);
-        bool within24Hours();
-    };
-
-    class Activity{
-        int type ;
-        std::string value ;
-    public : 
-        Activity();
-        Activity(int type, std::string value);
-        ~Activity() ;
-        void displayOptions();
-        void setTypeAndValue(int type, std :: string value) ; // sets the value given by the user and the type corresponding to the index in the table 
+        bool within24Hours(date other);
     };
 
     class user{
@@ -44,35 +33,50 @@ namespace dHeader{
         void removeFriend(user*u);
         void addLikedPage(page* p);
         void addPost(post* p);
-        void viewHome();
-        void likePost (); 
-        void viewProfile();
-        void viewFriendList();
+        user** getFriends();
+        page** getLikedPages();
+        post** getMyPosts();
+        int getFriendCount();
+        int getLikedPagesCount();
+        int getPostCount();
     };
 
     class post {
         std :: string ID ;
         std :: string description ;
         date shareDate ;
-        user ** usersThatLiked ; // max 10.  //if user had been deleted, it should be reduced from here
-        Activity *a ; // optional 
-        comment **comments ;      //max 10
+        user ** usersThatLiked ;// max 10.  //if user had been deleted, it should be reduced from here
+        comment **comments ;//max 10
         int likedCount ;
         int commentCount ; 
     public :
-        post(std::string id, std::string desc, date d,int type,std::string value);//: a(new Activity(type, value))
+        post(std::string id, std::string desc, date d);
         ~post() ;
         void addLike(user* u);
         void seeLiked() ; // list of people who have liked the post 
         void addComment(comment* c) ;
-        void view(); //getDate, getID, getDesc 
+        void view(); 
+        std :: string getID() ;
+        std :: string getDesc() ;
+        date getDate() ;
+    };
+
+    class Activity : public post{
+        int type ;
+        std::string value ;
+    public : 
+        Activity();
+        Activity(int type, std::string value);
+        ~Activity() ;
+        void displayOptions();
+        void setTypeAndValue(int type, std :: string value) ; // sets the value given by the user and the type corresponding to the index in the table 
     };
 
     class page {
         std :: string ID ;
         std:: string title;
         post** posts;
-        Memory **memories;
+        //Memory **memories;
         user* owner;
         int totalLiked;
         int postCount;
@@ -82,21 +86,28 @@ namespace dHeader{
         ~page();
         void addPost(post* p);
         void view() ; // displays all its posts 
+        std::string getID();
+        std::string getTitle();
+        post** getPosts();
+        int getPostCount();     
     };
 
     class comment{
-        user* commenter ; 
+        user* commenterUser;
+        page* commenterPage;
         std :: string text ;
     public :
         comment (user *commenter , std :: string text);
         ~comment ();
+        std::string getText();
+        user* getCommenterUser();
+        page* getCommenterPage();
         void view();
     };
 
-    class Memory{
+    class Memory : public post{
         post *original;
         std :: string text ;
-        date d;
     public : 
         Memory(post *original, std :: string text, date d) ;
         ~Memory();
@@ -104,7 +115,10 @@ namespace dHeader{
     };
 
     class socialNetworkApp{
-    
+        void viewHome();
+        void likePost (); 
+        void viewProfile();
+        void viewFriendList();  
 
     };// this class will serve the purpose of a driver program with the starting point socialNetworkApp :: run() 
 
