2b2466e52deff4f8ec0b4e7f3210bc62343a914c
MalikAbdul4078 <ar3456685@gmail.com>
2026-04-27 10:05:53 +0500

    blueprint of social network app, will modify if needed

---
+++ socialNetworkApp.h
@@ -0,0 +1,29 @@
+#pragma once 
+#include"headers.h"
+#include<fstream>
+namespace dHeader{
+class socialNetworkApp{
+    User **users ;
+    Page **pages ;
+    Post **posts ;
+    User *currentUser ;
+    public : 
+    void setCurrentUser(User *current) ;
+    void readUsers(const std :: istream &user);
+    void readPages(const std :: istream &pages);
+    void readPosts(const std :: istream &posts);
+    void setCurrentUser() ;
+    void viewHome() ;
+    void likeAPost();
+    void likedPeopleList() ; //displays the list of people who have liked a post 
+    void commentOnAPost() ;
+    void viewPost() ;
+    void shareMemory();
+    void viewProfile() ;
+    void viewFriendList() ;
+    void viewPage() ;
+    // all these functions will ask for the parameters first after they are called 
+    // this is done to avoid doing much work in the main and maintain the readability of the code 
+    
+};
+}
